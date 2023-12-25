8bf0b1afefa54ea53f64ae5eeb435577d61477e9
chocoboy08 <chocoboy0508@gmail.com>
2023-12-25 20:50:22 +0900

    build: :technologist: add prettier, eslint setting
    prettier 추가

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
+  arrowParens: 'always',
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  tabWidth: 2,
+  plugins: ['prettier-plugin-organize-imports'],
 };
