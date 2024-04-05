d80b4f09f44db26ebd436f5eaa4e66ad0820f9ac
chocoboy08 <chocoboy0508@gmail.com>
2024-04-05 18:47:49 +0900

    refactor: :recycle: change seemore link function
    더보기 버튼의 클릭 함수를 props로 변경했습니다.

--- components/SeeMore.tsx
+++ components/SeeMore.tsx
@@ -4,15 +4,15 @@ import IconRight from '../assets/icon_right.svg';
 import Typography from './@base/Typography';
 import React = require('react');
 interface SeeMoreProps {
-  link?: string;
+  routeFn: () => void;
 }
 
-function SeeMore({link}: SeeMoreProps) {
+function SeeMore({routeFn}: SeeMoreProps) {
   return (
     <Pressable
       style={{flexDirection: 'row', alignItems: 'center', gap: 4.5}}
       onPress={() => {
-        console.log(link);
+        routeFn();
       }}
     >
       <Typography variant="Info" color="#6f00f8">
